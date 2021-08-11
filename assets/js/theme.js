a106332ea103289c29783d70673b22f8650480e4
kjaunsleine <88329568+kjaunsleine@users.noreply.github.com>
2021-08-11 13:36:26 +0300

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,37 @@
 (function(){
+
+  'use strict';
+
+  function windowWidthCheck(){
+    if ($(window).width() < 992) {
+      $('.lang-menu').removeClass('dropdown');
+      $('.lang-menu').html('');
+      let html = '';
+      html += '<a class="nav-link active" href="#">LV</a>';
+      html += '<a class="nav-link" href="#">EN</a>';
+      html += '<a class="nav-link" href="#">RU</a>';
+      $('.lang-menu').html(html);
+    } 
+    else {
+      $('.lang-menu').addClass('dropdown');
+      $('.lang-menu').html('');
+      let html = '';
+      html += '<a class="nav-link navbar-toggler-right" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">LV</a>';
+      html += '<div class="dropdown-menu" aria-labelledby="navbarDropdown">';
+      html += '<a class="dropdown-item" href="#">EN</a>';
+      html += '<a class="dropdown-item" href="#">RU</a></div>';
+      $('.lang-menu').html(html);
+    }
+    } 
+
+    $(window).resize(windowWidthCheck);
+}());
+
+/* (function(){
   'use strict';
   
   function windowWidthCheck(){
-    if ($(window).width() < 770) {
+    if ($(window).width() < 992) {
     $('.lang-menu').removeClass('dropdown');
     $('.lang-menu').html('');
     let html = '';
@@ -12,5 +41,7 @@
     $('.lang-menu').html(html);
     } 
   }
+
+  windowWidthCheck();
   
-}());
+}()); */
